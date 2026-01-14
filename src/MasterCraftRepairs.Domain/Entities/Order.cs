4c067fb6671ca0cfc54cf8b10860c7038557af68
D4cLoves <vladislavvtr12@mail.ru>
2026-01-15 01:47:56 +0300

    add description column for order table

--- src/MasterCraftRepairs.Domain/Entities/Order.cs
+++ src/MasterCraftRepairs.Domain/Entities/Order.cs
@@ -5,82 +5,131 @@ namespace MasterCraftRepairs.Domain.Entities;
 
 public abstract class Order
 {
-     public Guid Id { get; protected set; } = Guid.NewGuid();
+    public Guid Id { get; protected set; } = Guid.NewGuid();
 
-     public Guid ProductId { get; private set; }
-     public Guid MasterId { get; private set; }
-     public Guid ClientId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public Guid? MasterId { get; private set; }
+    public Guid ClientId { get; private set; }
 
-     public Product Product { get; private set; } = null!;
-     public Master Master { get; private set; } = null!;
-     public Client Client { get; private set; } = null!;
+    public Product Product { get; private set; } = null!;
+    public Master? Master { get; private set; }
+    public Client Client { get; private set; } = null!;
 
-     public DateTime StartDate { get; protected set; }
-     public DateTime? EndDate { get; protected set; }
+    public DateTime StartDate { get; protected set; }
+    public DateTime? EndDate { get; protected set; }
 
-     public Money Price { get; private set; }
+    public Money Price { get; private set; }
 
-     protected Order() { }
+    public Description DescriptionOrder { get; private set; } = null!;
 
-     protected Order(Guid productId, Guid masterId, Guid clientId, decimal price)
-     {
-         ProductId = productId;
-         MasterId = masterId;
-         ClientId = clientId;
-         StartDate = DateTime.UtcNow;
-         Price = new  Money(price);
-     }
+    protected Order() { }
 
-     public abstract Order PutIntoWork(Guid masterId);
-     public abstract Order Complete(DateTime endDate);
-     public abstract Order Cancel();
+    protected Order(
+        Guid productId,
+        Guid? masterId,
+        Guid clientId,
+        decimal price,
+        Description descriptionOrder
+    )
+    {
+        ProductId = productId;
+        MasterId = masterId;
+        ClientId = clientId;
+        StartDate = DateTime.UtcNow;
+        Price = new Money(price);
+        DescriptionOrder = descriptionOrder;
+    }
+
+    public abstract Order PutIntoWork(Guid masterId);
+    public abstract Order Complete(DateTime endDate);
+    public abstract Order Cancel();
 }
 
 public class NewOrder : Order
 {
     private NewOrder() { } // For EF Core
 
-    internal NewOrder(Guid productId, Guid masterId, Guid clientId, decimal price) : base(productId, masterId, clientId, price)
-    {
-    }
+    internal NewOrder(
+        Guid productId,
+        Guid? masterId,
+        Guid clientId,
+        decimal price,
+        Description descriptionOrder
+    )
+        : base(productId, masterId, clientId, price, descriptionOrder) { }
 
-    public override Order PutIntoWork(Guid masterId) => new OrderInProgress(Id, ProductId, masterId, ClientId, Price, StartDate);
+    public override Order PutIntoWork(Guid masterId) =>
+        new OrderInProgress(Id, ProductId, masterId, ClientId, Price, StartDate, DescriptionOrder);
+
     public override Order Complete(DateTime endDate) => throw new InvalidOperationException("нет");
-    public override Order Cancel() => new CancelledOrder(Id, ProductId, MasterId, ClientId, Price, StartDate);
+
+    public override Order Cancel() =>
+        new CancelledOrder(Id, ProductId, MasterId, ClientId, Price, StartDate, DescriptionOrder);
 }
 
 public class OrderInProgress : Order
 {
     private OrderInProgress() { } // For EF Core
 
-    public OrderInProgress(Guid id, Guid productId, Guid masterId, Guid clientId, Money price, DateTime startDate) : base(
-        productId, masterId, clientId, price.Amount)
+    public OrderInProgress(
+        Guid id,
+        Guid productId,
+        Guid masterId,
+        Guid clientId,
+        Money price,
+        DateTime startDate,
+        Description descriptionOrder
+    )
+        : base(productId, masterId, clientId, price.Amount, descriptionOrder)
     {
         Id = id;
         StartDate = startDate;
     }
 
     public override Order PutIntoWork(Guid masterId) => this;
-    public override Order Complete(DateTime endDate) => new CompletedOrder(Id, ProductId, MasterId, ClientId, Price, StartDate, endDate);
-    public override Order Cancel() => new CancelledOrder(Id, ProductId, MasterId, ClientId, Price, StartDate);
 
+    public override Order Complete(DateTime endDate) =>
+        new CompletedOrder(
+            Id,
+            ProductId,
+            MasterId,
+            ClientId,
+            Price,
+            StartDate,
+            endDate,
+            DescriptionOrder
+        );
+
+    public override Order Cancel() =>
+        new CancelledOrder(Id, ProductId, MasterId, ClientId, Price, StartDate, DescriptionOrder);
 }
 
 public class CompletedOrder : Order
 {
     private CompletedOrder() { } // For EF Core
 
-    public CompletedOrder(Guid id, Guid productId, Guid masterId, Guid clientId, Money price, DateTime startDate,
-        DateTime endDate) : base(
-        productId, masterId, clientId, price.Amount)
+    public CompletedOrder(
+        Guid id,
+        Guid productId,
+        Guid? masterId,
+        Guid clientId,
+        Money price,
+        DateTime startDate,
+        DateTime endDate,
+        Description descriptionOrder
+    )
+        : base(productId, masterId, clientId, price.Amount, descriptionOrder)
     {
         Id = id;
         StartDate = startDate;
-        EndDate =  endDate;
+        EndDate = endDate;
     }
 
-    public override Order PutIntoWork(Guid masterId) => throw new InvalidOperationException("нет, он уже завершен");
+    public override Order PutIntoWork(Guid masterId) =>
+        throw new InvalidOperationException("нет, он уже завершен");
+
     public override Order Complete(DateTime endDate) => this;
+
     public override Order Cancel() => throw new InvalidOperationException("нет, он уже завершен");
 }
 
@@ -88,14 +137,26 @@ public class CancelledOrder : Order
 {
     private CancelledOrder() { } // For EF Core
 
-    public CancelledOrder(Guid id, Guid productId, Guid masterId, Guid clientId, Money price, DateTime startDate) : base(
-        productId, masterId, clientId, price.Amount)
+    public CancelledOrder(
+        Guid id,
+        Guid productId,
+        Guid? masterId,
+        Guid clientId,
+        Money price,
+        DateTime startDate,
+        Description descriptionOrder
+    )
+        : base(productId, masterId, clientId, price.Amount, descriptionOrder)
     {
         Id = id;
         StartDate = startDate;
     }
 
-    public override Order PutIntoWork(Guid masterId) => throw new InvalidOperationException("нет, он уже отменен");
-    public override Order Complete(DateTime endDate) => throw new InvalidOperationException("нет, он уже отменен");
+    public override Order PutIntoWork(Guid masterId) =>
+        throw new InvalidOperationException("нет, он уже отменен");
+
+    public override Order Complete(DateTime endDate) =>
+        throw new InvalidOperationException("нет, он уже отменен");
+
     public override Order Cancel() => this;
 }
--- src/MasterCraftRepairs.Infrastructure/Entities/20260114224553_add description column.Designer.cs
+++ src/MasterCraftRepairs.Infrastructure/Entities/20260114224553_add description column.Designer.cs
@@ -4,16 +4,19 @@ using MasterCraftRepairs.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace MasterCraftRepairs.Infrastructure.Entities
+namespace MasterCraftRepairs.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260114224553_add description column")]
+    partial class adddescriptioncolumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
